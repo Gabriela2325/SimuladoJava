97df76b37c23f4e81837dd1ab8ad8bc4733ea583
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-06-25 11:37:49 -0300

    Setup

---
+++ src/Programa.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import models.*;
+
+public class Programa {
+    public static void main(String[] args) throws FileNotFoundException {
+        
+    }
+}
